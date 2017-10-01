3fe5b100d135161795598fd8959059f6688b220c
40206111 <40206111@live.napier.ac.uk>
2017-10-01 01:37:15 +0100

    Particle Assesment -An actual attempt at a blowdryer in the blowdryer main now

--- project2/Mains/4Blowdryer/main.cpp
+++ project2/Mains/4Blowdryer/main.cpp
@@ -28,7 +28,7 @@
 
 
 // time
-const double dt = 0.01;
+const GLfloat dt = 0.01f;
 double currentTime = glfwGetTime();
 double accumulator = 0.0f;
 
@@ -50,25 +50,47 @@ int main()
 
 	Shader pShader = Shader("resources/shaders/core.vert", "resources/shaders/core_blue.frag");
 
-
-	// create particle
-	Particle p2 = Particle::Particle();
-	p2.getMesh().setShader(pShader);
-	p2.setPos(glm::vec3(-1.0f, 2.0f, 0.0f));
-
-	// create particle
-	Particle p[2];
-	glm::vec3 r[2];
+	// create particles
+	int amount = 2;
+	std::vector<Particle> p(amount);
 	glm::vec3 g = glm::vec3(0.0f, -9.8f, 0.0f);
+	float energy_loss = 0.9f;
+	std::vector<glm::vec3> fg(amount);
+	std::default_random_engine generate;
+	std::uniform_real_distribution<float> place(-2.5f, 2.5f);
+	std::uniform_real_distribution<float> placey(0.5f, 5.0f);
+
+	for (int i = 0; i < amount; i++)
+	{
+		p[i] = Particle::Particle();
+		//p[i].setVel(glm::vec3(1.0f, 0.0f, 1.0f));
+		p[i].getMesh().setShader(pShader);
+		p[i].setPos(glm::vec3(0.0f, 4.9f, 0.0f));
+		fg[i] = p[i].getMass() * g;
+	}
+	p[0].setPos(0, 2.0f);
+	p[1].setPos(0, 0.0f);
+
+	//Define Box
 	glm::vec3 o = glm::vec3(-2.5f, 0.0f, -2.5f);
 	glm::vec3 d = glm::vec3(5.0f, 5.0f, 5.0f);
 
-	for (int i = 0; i < 2; i++)
-	{
-		p[i] = Particle::Particle();
-		p[i].getMesh().setShader(pShader);
-		p[i].setPos(glm::vec3((p2.getPos()[0] + i + 1), 2.0f, 0.0f));
-	}
+	///DEFINE CONE///
+	//Top and bottom
+	glm::vec3 Ct = glm::vec3(0.0f, 2.5f, 0.0f);
+	glm::vec3 Cb = glm::vec3(0.0f);
+	//top and bottom radius
+	float Ctr = 1.25f;
+	float Cbr = 0.675f;
+	//Cone total hieght if it went to a point
+	float Ch = (Ctr*Ct.y) / (Cbr*((Ctr / Cbr) - 1));
+	//Where the cone would be a point if it got that far
+	glm::vec3 point = glm::vec3(Ct);
+	point.y -= Ch;
+	//force
+	glm::vec3 Fc = glm::vec3(0.0f);
+	//force coefficient
+	float coneForceCo = 5.0f;
 
 
 	// Game loop
@@ -83,46 +105,93 @@ int main()
 		/*
 		**	INTERACTION
 		*/
-		// Manage interaction
-		app.doMovement(dt);
+
 
 		while (accumulator > dt)
 		{
+			// Manage interaction
+			app.doMovement(dt);
 
 			/*
 			**	SIMULATION
 			*/
-			//SEMI-IMPLICIT EULER
-			glm::vec3 v = p[0].getVel();
-			glm::vec3 r = p[0].getPos();
-			p[0].setAcc(g);
-			v += dt * p[0].getAcc();
-			r += dt * p[0].getVel();
-			p[0].setPos(r);
-			p[0].setVel(v);
 
-			//FORWARD EULER
-			v = p[1].getVel();
-			r = p[1].getPos();
-			p[1].setAcc(g);
-			v += dt * p[1].getAcc();
-			r += dt * p[1].getVel();
-			p[1].setPos(r);
-			p[1].setVel(v);
-
-			for (int j = 0; j < 2; j++)
+			for (int i = 0; i < amount; i++)
 			{
-				for (int i = 0; i < 3; i++)
+
+				glm::vec3 v = p[i].getVel();
+				glm::vec3 r = p[i].getPos();
+
+				//Check if in cone height 
+				if (r.y <= Ct.y && r.y >= Cb.y)
 				{
-					if (p[j].getPos()[i] <= o[i])
+					//cone at the hieght of the particle
+					glm::vec3 currentH = glm::vec3(Ct);
+					currentH.y = r.y;
+					//radius of cone at height
+					float coneR = Cbr + (currentH.y / Ct.y)*(Ctr - Cbr);
+					//distance from center to particle
+					float newR = glm::length(currentH - r);
+
+					//check if in cone radius
+					if (newR < coneR)
 					{
-						p[j].setPos(i, o[i]);
-						p[j].setVel(i, p[j].getVel()[i] * -1);
+						//direction of force
+						glm::vec3 fdir = r - point;
+						//radius to point at top of cone
+						float topR = (newR / (Ct.y - currentH.y))*Ct.y;
+						//point at top at new top radius
+						glm::vec3 topPoint = Ct;
+						topPoint.x = Ct.x + topR;
+						//vector from point to top of cone through particle
+						glm::vec3 dirTop = glm::vec3(topPoint - point);
+						Fc = ((dirTop - fdir) * (coneR - newR))*coneForceCo;
+						//Fc = glm::vec3(-3.0f, 3.0f, 0.0f);
 					}
-					else if (p[j].getPos()[i] >= (o[i] + d[i]))
+					else
 					{
-						p[j].setPos(i, o[i] + d[i]);
-						p[j].setVel(i, p[j].getVel()[i] * -1);
+						//set Cone force to zero
+						Fc *= 0;
+					}
+
+				}
+				else
+				{
+					//set cone force to zero
+					Fc *= 0;
+				}
+
+				//calculate drag
+				glm::vec3 drag = 0.5 * 1.225 * -v * glm::length(v) * 1.05 * 0.01;
+				//total force
+				glm::vec3 F = drag + fg[i] + Fc;
+				if (i == 1)
+				{
+					//std::cout << Fc.x << ", " << Fc.y << ", " << Fc.z << std::endl;
+				}
+				//calculate acceleration
+				p[i].setAcc((F) / p[i].getMass());
+				//semi implicit Eular
+				v += dt * p[0].getAcc();
+				r += dt * v;
+				//set postition and velocity
+				p[i].setPos(r);
+				p[i].setVel(v);
+
+				//check if in box
+				for (int j = 0; j < 3; j++)
+				{
+					if (p[i].getPos()[j] <= o[j])
+					{
+						p[i].setPos(j, o[j]);
+						p[i].setVel(j, p[i].getVel()[j] * -1);
+						p[i].setVel(p[i].getVel() * energy_loss);
+					}
+					else if (p[i].getPos()[j] >= (o[j] + d[j]))
+					{
+						p[i].setPos(j, o[j] + d[j]);
+						p[i].setVel(j, p[i].getVel()[j] * -1);
+						p[i].setVel(p[i].getVel() * energy_loss);
 					}
 				}
 			}
@@ -139,10 +208,10 @@ int main()
 		// draw groud plane
 		app.draw(plane);
 		// draw particles
-		app.draw(p[0].getMesh());
-		app.draw(p[1].getMesh());
-		app.draw(p2.getMesh());
-
+		for (int i = 0; i < amount; i++)
+		{
+			app.draw(p[i].getMesh());
+		}
 		app.display();
 	}
 
